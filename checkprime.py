1647592f1f2d0439e79f89ff9618bb776b133428
ramesh2901 <rameshkoritikanti@gmail.com>
2017-08-31 17:25:25 -0700

    file is added

---
+++ checkprime.py
@@ -0,0 +1,10 @@
+num = int(input("Enter your number"))
+if num >1:
+	for i in range(2,num):
+		if num % i == 0:
+			print(num,"is not prime number")
+			break 
+	else:
+		print(num,"is a prime number")
+	
+
